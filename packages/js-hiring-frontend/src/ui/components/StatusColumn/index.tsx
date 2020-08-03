d9b17a9340fe477e62c15aa5e4f0199ee3df47ae
Nick Cunningham <nick.cunningham@leadcrunch.ai>
2020-08-02 17:02:10 -0700

    make the status columns prettier

--- packages/js-hiring-frontend/src/ui/components/StatusColumn/index.tsx
+++ packages/js-hiring-frontend/src/ui/components/StatusColumn/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Col } from "reactstrap";
 import { TaskCard } from "../TaskCard";
+import styled from "styled-components";
+
+const StyledCol = styled(Col)`
+    background: #fefefe;
+    border: 1px dashed #dddddd;
+    min-width: 200px;
+    min-height: 300px;
+`;
 
 export const StatusColumn = ({ status, tasks }: any) => {
     return (
-        <Col>
+        <StyledCol>
             <h2>{status}</h2>
             {tasks.map(
                 (task: any) =>
@@ -18,6 +26,6 @@ export const StatusColumn = ({ status, tasks }: any) => {
                         />
                     )
             )}
-        </Col>
+        </StyledCol>
     );
 };
